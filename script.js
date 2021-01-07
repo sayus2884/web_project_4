a2fa6b12088263b36267115997245f59cd28a46e
joniah2884 <>
2021-01-07 00:39:31 -0800

    feature: edit name and job and reflect to profile

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 let page = document.querySelector(".page");
+
+let profile = page.querySelector(".profile");
+let nameProfile = page.querySelector(".profile__name");
+let jobProfile = page.querySelector(".profile__job");
+
 let edit = page.querySelector(".edit");
-let editButton = page.querySelector(".profile__edit-button");
-let closeButton = page.querySelector(".edit__close");
+let nameInput = edit.querySelector(".edit__item_type_name");
+let jobInput = edit.querySelector(".edit__item_type_job");
+
+let person = {
+  name: "Mike Izenhower",
+  job: "Priest"
+};
 
 function openEditModal(){
   edit.classList.remove("edit_hidden");
@@ -11,5 +21,27 @@ function closeEditModal(){
   edit.classList.add("edit_hidden");
 }
 
-editButton.addEventListener("click", openEditModal);
-closeButton.addEventListener("click", closeEditModal);
+function setProfile(){
+  nameProfile.innerHTML = nameInput.value;
+  jobProfile.innerHTML = jobInput.value;
+
+  closeEditModal();
+}
+
+function init(){
+
+  let editButton = profile.querySelector(".profile__edit-button");
+  let closeButton = edit.querySelector(".edit__close");
+  let saveButton = edit.querySelector(".edit__save-button");
+
+  editButton.addEventListener("click", openEditModal);
+  closeButton.addEventListener("click", closeEditModal);
+  saveButton.addEventListener("click", setProfile);
+
+  nameInput.value = person.name;
+  jobInput.value = person.job;
+
+  setProfile();
+}
+
+init();
