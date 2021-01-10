60b3a0b5bca87700e809caa68a1df6f71bb64aec
joniah2884 <>
2021-01-09 17:22:41 -0800

    hotfix: name and job change on submit event

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-let body = document.querySelector("body");
-let page = document.querySelector(".page");
+let profileContainer = document.querySelector(".profile");
+let nameProfile = profileContainer.querySelector(".profile__name");
+let jobProfile = profileContainer.querySelector(".profile__job");
 
-let profile = page.querySelector(".profile");
-let nameProfile = page.querySelector(".profile__name");
-let jobProfile = page.querySelector(".profile__job");
+let placesContainer = document.querySelector(".places");
 
-let places = page.querySelector(".places");
-
-let edit = page.querySelector(".edit");
-let nameInput = edit.querySelector(".edit__item_type_name");
-let jobInput = edit.querySelector(".edit__item_type_job");
+let editContainer = document.querySelector(".edit");
+let nameInput = editContainer.querySelector(".edit__item_type_name");
+let jobInput = editContainer.querySelector(".edit__item_type_job");
 
 let person = {
   name: 'Philliney Chandler',
@@ -17,32 +14,33 @@ let person = {
 };
 
 function openEditModal(){
-  edit.classList.remove("edit_hidden");
-  body.style.overflow = "hidden";
+  nameInput.value = nameProfile.textContent;
+  jobInput.value = jobProfile.textContent;
+
+  editContainer.classList.remove("edit_hidden");
 }
 
 function closeEditModal(){
-  edit.classList.add("edit_hidden");
-  body.style.overflow = "auto";
+  editContainer.classList.add("edit_hidden");
 }
 
-function setProfile(){
-  nameProfile.innerHTML = nameInput.value;
-  jobProfile.innerHTML = jobInput.value;
+function setProfile(event){
+  event.preventDefault();
+
+  nameProfile.textContent = nameInput.value;
+  jobProfile.textContent = jobInput.value;
 
   closeEditModal();
 }
 
 function toggleHeart(heartDOM){
   return function(){
-    let isDisabled = heartDOM.classList.contains("places__heart_disabled");
+    let isActive = heartDOM.classList.contains("places__heart_active");
 
-    if (isDisabled) {
-      heartDOM.classList.add("places__heart_active");
-      heartDOM.classList.remove("places__heart_disabled");
-    } else {
-      heartDOM.classList.add("places__heart_disabled");
+    if (isActive) {
       heartDOM.classList.remove("places__heart_active");
+    } else {
+      heartDOM.classList.add("places__heart_active");
     }
 
   }
@@ -50,11 +48,11 @@ function toggleHeart(heartDOM){
 
 function init(){
 
-  let editButton = profile.querySelector(".profile__edit-button");
-  let closeButton = edit.querySelector(".edit__close-button");
-  let saveButton = edit.querySelector(".edit__save-button");
+  let editButton = profileContainer.querySelector(".profile__edit-button");
+  let closeButton = editContainer.querySelector(".edit__close-button");
+  let editForm = editContainer.querySelector(".edit__form");
 
-  let hearts = places.querySelectorAll(".places__heart");
+  let hearts = placesContainer.querySelectorAll(".places__heart");
 
   for (var i = 0; i < hearts.length; i++) {
     hearts[i].classList.add("places__heart_disabled");
@@ -63,12 +61,10 @@ function init(){
 
   editButton.addEventListener("click", openEditModal);
   closeButton.addEventListener("click", closeEditModal);
-  saveButton.addEventListener("click", setProfile);
+  editForm.addEventListener("submit", setProfile);
 
-  nameInput.value = person.name;
-  jobInput.value = person.job;
-
-  setProfile();
+  nameProfile.textContent = person.name;
+  jobProfile.textContent = person.job;
 }
 
 init();
