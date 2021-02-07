d6968d6ab25e1c7033330671fd40a86d2f2f5ed7
bakagai <>
2021-02-06 20:46:01 -0800

    feature: add validation to forms

--- scripts/index.js
+++ scripts/index.js
@@ -17,6 +17,8 @@ const imagePopupContainer = document.querySelector(".image-popup");
 const imageElement = imagePopupContainer.querySelector(".image-popup__image");
 const titleElement = imagePopupContainer.querySelector(".image-popup__title");
 
+const forms = document.querySelectorAll(".modal__form")
+
 function setProfile(data){
   nameProfile.textContent = data.name;
   jobProfile.textContent = data.job;
@@ -61,6 +63,63 @@ function openPopup(popup, hiddenClassName="modal_hidden"){
   popup.classList.toggle(hiddenClassName);
 }
 
+function showError(formElement, inputElement, errorMessage) {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  errorElement.classList.add("form__input-error_active");
+  errorElement.textContent = errorMessage;
+  inputElement.classList.add("form__input_error_active");
+}
+
+function hideError(formElement, inputElement) {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  errorElement.classList.remove("form__input-error_active");
+  inputElement.classList.remove("form__input_error_active");
+}
+
+function checkInputValidity(formElement, inputElement) {
+  !inputElement.validity.valid ?
+    showError(formElement, inputElement, inputElement.validationMessage) :
+    hideError(formElement, inputElement);
+}
+
+function hasInvalidInput(inputList) {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  })
+}
+
+function toggleButtonState(inputList, buttonElement) {
+  hasInvalidInput(inputList) ?
+  buttonElement.classList.add("form__submit-button_inactive") :
+  buttonElement.classList.remove("form__submit-button_inactive")
+}
+
+function setInputEventListener(formElement) {
+  const inputList = Array.from(formElement.querySelectorAll(".form__input"));
+  const submitButton = formElement.querySelector(".form__submit-button");
+
+  toggleButtonState(inputList, submitButton);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", (event) => {
+      checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, submitButton);
+    })
+  });
+}
+
+function enableValidation() {
+  const formList = Array.from(document.querySelectorAll(".form"));
+  formList.forEach((formElement) => {
+    formElement.addEventListener("submit", (event) => {
+      event.preventDefault();
+    });
+
+    setInputEventListener(formElement)
+  });
+
+}
+
 function handleEditSubmit(event){
   event.preventDefault();
   setProfile({ name: nameInput.value, job: jobInput.value })
@@ -75,7 +134,6 @@ function handleAddSubmit(event){
 
   openPopup(addModal);
 }
-
 
 function init(){
 
@@ -104,9 +162,13 @@ function init(){
     nameInput.value = nameProfile.textContent;
     jobInput.value = jobProfile.textContent;
     openPopup(editModal);
+    enableValidation();
   });
 
-  addButton.addEventListener("click", () => openPopup(addModal));
+  addButton.addEventListener("click", () => {
+    openPopup(addModal)
+    enableValidation();
+  });
 
   closeEditButton.addEventListener("click", () => openPopup(editModal));
   closeAddButton.addEventListener("click", () => openPopup(addModal));
